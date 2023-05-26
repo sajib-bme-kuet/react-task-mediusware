2975c80ef5caf396364ade5dca246a4923ebe23d
Md. Shohidujjaman Sajib <sajibbmekuet@gmail.com>
2023-05-26 21:36:44 +0600

    Modal Layout ok

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,23 +1,44 @@
-import React from 'react';
+import { useState } from "react";
+import Modal from "./problem_2/Modal";
 
 const Problem2 = () => {
 
+    const [showModalA, setShowModalA] = useState(false)
+    const [showModalB, setShowModalB] = useState(false)
+    const handleCloseA = () => setShowModalA(false)
+    const handleCloseB = () => setShowModalB(false)
+
+    const handleModalOpen = (id) => {
+        if (id === 'A') {
+            setShowModalA(true)
+            setShowModalB(false)
+        } else {
+            setShowModalB(true)
+            setShowModalA(false)
+        }
+    }
     return (
 
         <div className="container"  >
-        
+
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
 
 
-                <div style={{position: "absolute",top: "50%",left: "50%", transform: "translate(-50%, -50%)"}}>
-                <div className="d-flex justify-content-center gap-3">
-                    <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                    <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
-                </div>
+                <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
+                    <div className="d-flex justify-content-center gap-3">
+                        <button className="btn btn-lg text-light" type="button" onClick={()=>handleModalOpen("A")} style={{backgroundColor:"#46139f"}} >All Contacts</button>
+                        <button className="btn btn-lg text-light" type="button" onClick={()=>handleModalOpen("B")} style={{backgroundColor:"#ff7f50"}}>US Contacts</button>
+                    </div>
                 </div>
 
 
+                <Modal title="Modal A" handleClose={handleCloseA} show={showModalA} modalSwitcher={handleModalOpen}>
+                    
+                </Modal>
+                <Modal title="Modal B" handleClose={handleCloseB} show={showModalB} modalSwitcher={handleModalOpen}>
+                    
+                </Modal>
             </div>
         </div>
     );
---
+++ src/components/problem_2/Modal.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Modal = ({ title, show, handleClose, children, modalSwitcher }) => {
+  return (
+    <div className={`modal ${show ? 'show' : ''}`} style={{ display: show ? 'block' : 'none' }}>
+      <div className="modal-dialog modal-lg">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">{title}</h5>
+            <button type="button" className="btn" onClick={handleClose}>Close</button>
+          </div>
+          <div className="modal-body">
+
+            <div className="d-flex justify-content-center gap-3">
+              <button className="btn btn-md text-light" type="button" onClick={() => modalSwitcher("A")} style={{ backgroundColor: "#46139f" }} >All Contacts</button>
+              <button className="btn btn-md text-light" type="button" onClick={() => modalSwitcher("B")} style={{ backgroundColor: "#ff7f50" }}>US Contacts</button>
+            </div>
+
+            {children}</div>
+          <div className="modal-footer justify-content-start">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="only-even" />
+              <label class="form-check-label" for="only-even">
+                Only Even
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
