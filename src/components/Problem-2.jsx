aea5392f7d903df437bc36600845161cbd5728d0
Md. Shohidujjaman Sajib <sajibbmekuet@gmail.com>
2023-05-26 21:00:08 +0600

    centered the div

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -4,15 +4,20 @@ const Problem2 = () => {
 
     return (
 
-        <div className="container">
+        <div className="container"  >
+        
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
+
+
+                <div style={{position: "absolute",top: "50%",left: "50%", transform: "translate(-50%, -50%)"}}>
                 <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
+                    <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
+                    <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
                 </div>
-                
+                </div>
+
+
             </div>
         </div>
     );
